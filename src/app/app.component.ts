24765c4ac72d71fb029cd32095807f8b11b9ee4c
saqlainAbbas <saqlain.abbas@narsun.pk>
2022-01-21 18:39:55 +0500

    video box is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
-import { Component } from '@angular/core';
-import {animate, AUTO_STYLE, state, style, transition, trigger} from "@angular/animations";
-
-let DEFAULT_DURATION = 250;
-
+import {Component, ViewChild} from '@angular/core';
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  // animations: [
-  //   trigger('collapse', [
-  //     state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
-  //     state('true', style({ height: '0', display: 'none' })),
-  //     transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
-  //     transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
-  //   ])
-  // ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('videoPlayer') videoPlayer: any;
+  modal: any;
   title = 'cloudCity';
   collapsed = false;
   collapseArr = [
@@ -38,15 +29,26 @@ export class AppComponent {
     {
       collapse: true
     }
-  ]
+  ];
+
+  constructor(private modalService: NgbModal) {
+  }
+
   toggle(i: number) {
     this.collapseArr[i].collapse = !this.collapseArr[i].collapse;
   }
+
   closeRest(i: number) {
     this.collapseArr.forEach((val, index) => {
       if (index !== i) {
         val.collapse = true;
       }
     })
+  }
+  onVideoClicked(): void {
+    this.modal = this.modalService.open(this.videoPlayer,
+      {
+        centered: true
+      });
   }
 }
