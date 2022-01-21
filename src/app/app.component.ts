735b52c1b8441c5a75239743c639bdfbe29eb344
saqlainAbbas <saqlain.abbas@narsun.pk>
2022-01-21 16:25:27 +0500

    collpase able completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,39 +7,46 @@ let DEFAULT_DURATION = 250;
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  animations: [
-    trigger('collapse', [
-      state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
-      state('true', style({ height: '0', display: 'none' })),
-      transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
-      transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
-    ])
-  ]
+  // animations: [
+  //   trigger('collapse', [
+  //     state('false', style({ height: AUTO_STYLE, visibility: AUTO_STYLE })),
+  //     state('true', style({ height: '0', display: 'none' })),
+  //     transition('false => true', animate(DEFAULT_DURATION + 'ms ease-in')),
+  //     transition('true => false', animate(DEFAULT_DURATION + 'ms ease-out'))
+  //   ])
+  // ]
 })
 export class AppComponent {
   title = 'cloudCity';
   collapsed = false;
   collapseArr = [
     {
-      collapse: false
+      collapse: true
     },
     {
-      collapse: false
+      collapse: true
     },
     {
-      collapse: false
+      collapse: true
     },
     {
-      collapse: false
+      collapse: true
     },
     {
-      collapse: false
+      collapse: true
     },
     {
-      collapse: false
+      collapse: true
     }
   ]
   toggle(i: number) {
     this.collapseArr[i].collapse = !this.collapseArr[i].collapse;
+  }
+  closeRest(i: number) {
+    this.collapseArr.forEach((val, index) => {
+      if (index !== i) {
+        val.collapse = true;
+      }
+    })
   }
 }
